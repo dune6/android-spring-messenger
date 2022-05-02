d8d28bd7ce9ef727acb7140f282668ac7ad309ba
dune6 <ilya.voznesensky@gmail.com>
2022-05-02 23:41:23 +0300

    Create a Spring project

---
+++ spring-web-api/src/main/kotlin/com/dune6/springwebservice/SpringWebServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.dune6.springwebservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringWebServiceApplication>(*args)
+}
